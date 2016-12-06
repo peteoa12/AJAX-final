e288e45a3064a94fa092eb06b4f5e81925f4c460
peteoa12 <peter.norman@atlanta.creativecircus.edu>
2016-12-06 15:47:46 -0500

    Added Google places API

--- js/main.js
+++ js/main.js
@@ -27,23 +27,50 @@ var BandsApi = (function(options){
           dataType: 'jsonp'
         })
         .done(function(data) {
-          // console.log("success",data);
-          displayDates(data);
+          console.log(data);
+
+          if(data.hasOwnProperty("errors")) {
+            $("body").append(
+             '<div>Never heard of them before!</div>')
+
+          } else if(Array.isArray(data) && !data.length) {
+             $("body").append(
+              '<div>Sorry, there are no dates for this artist!</div>')
+
+          } else if(Array.isArray(data) && data.length) {
+            displayDates(data);
+            $("body").append(
+              '<div>Here you go!</div>')
+
+          } else if(typeof data == "object" && data.mbid == null ){
+            $("body").append(
+              '<div>No artist specified.</div>')
+
+          } else {
+            $("body").append(
+              '<div>Unknown error.</div>')
+          }
+          
+
+
         });
-        //Remember to install the widget
 
         return false;
     });
   }
 
-   function displayDates(data, result) {
+  function errorHandeling(data, artist) {
+
+  }
+
+   function displayDates(data, result,$results) {
 
     for (var i = 0; i < data.length; i++) {
         var r = data[i];
         console.log(r);
        
         if(r.venue){
-          
+  
           GoogleMapApi.createMarker({
             geometry: {
               location: { 
@@ -53,10 +80,12 @@ var BandsApi = (function(options){
             },
             title: r.venue.name,
             url:r.url,
-            date:new Date(r.datetime,['America/New_York']).toLocaleString()  
+            date:new Date(r.datetime),
+            tickets:r.ticket_url
           });
         
         }
+
       }
     }
 
@@ -99,15 +128,20 @@ var GoogleMapApi = (function(options, r){
     });
     
     google.maps.event.addListener(marker, 'click', function() {
+
+        GooglePlacesApi.search({
+           latitude: result.geometry.location.lat,
+           longitude: result.geometry.location.lng
+        });
+
         createInfoWindow(result, marker);
         infowindow.open(map, this);
     });
   };
 
   function createInfoWindow(result, marker) {
-    console.log(r);
     var contentString = `<h3 class="marker-title">Venue: ${result.title}<h3>
-    <p class="show-date">${result.date}</p>`
+    <p class="show-date">${result.date}</p><a href="${result.tickets}" target="_blank" class="tickets"><img src="assets/img/movie-tickets.png" alt="tickets"></a>`
     infowindow.setContent(contentString);
   };
 
@@ -120,42 +154,38 @@ var GoogleMapApi = (function(options, r){
 
 //=========================================================================================================//
 
-//---------------------------------------------Hotwire-------------------------------------------------//
+//---------------------------------------------Air BnB-------------------------------------------------//
 
 //=========================================================================================================//
 
-var HotwireApi = (function(options){
-
+var GooglePlacesApi = (function(){
+  
   var shared = {};
-  var options = options || {};
-     
-  function setupListeners() {
-    setupSearch();
-  }
 
-  function setupSearch(){
-    var url = 'http://api.hotwire.com/v1/tripstarterl/hotel?apikey=n9ktvyrgwumm63b5ddrkc98w';
+
+  function search(data){
+    var latitude = data.latitude;
+    var longitude = data.longitude;
+    var url = "php/googlePlaces.php?location="+latitude+','+longitude+"";
 
       $.ajax({
         url: url,
         method: 'GET',
-        dataType: 'jsonp'
+        dataType: 'json'
       })
       .done(function(data) {
         console.log("success", data);
-      })      
+      }).fail(function(jqXHR, textStatus, errorThrown) {
+        console.log(jqXHR, textStatus, errorThrown)
+      })
   }
-
-  var init = function() {
-    setupListeners();
-  };
-  shared.init = init;
+  shared.search = search;
 
   return shared;
 
 
 }());
-HotwireApi.init();
+
 
 
 
